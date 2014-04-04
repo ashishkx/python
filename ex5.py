c30da4bc1bd1dba7ff84cfcb21248692c476bf59
Ashish Kumar <ashish@Ashish.local>
2014-04-04 21:02:57 +0530

    next few

---
+++ ex5.py
@@ -0,0 +1,16 @@
+my_name = 'Ashish'
+my_age = 18 #almost 19
+my_height = 167 # cm
+my_weight = 60 # kg
+my_eyes = 'grey'
+my_teeth = 'white'
+my_hair =  'dark brown'
+
+print "Lets talk about %s" %my_name
+print "He is %d cm tall" %my_height
+print "He is %d kg" %my_weight
+print "Hes got %s teeth and %s hair" %(my_teeth, my_hair)
+
+#this is tricky
+
+print " If I add %d, %d, and %d I get %d: " %(my_age, my_height, my_weight, my_age + my_height + my_weight)
